e62175f39f24113153d1f09e3d08fc125c9df0ad
avrsanjay <avrsanjay@gmail.com>
2016-02-24 17:09:33 -0800

    Final custom Stock Data Generator for Kafka

--- src/main/java/com/cloudwick/kafka/producer/TestProducer.java
+++ src/main/java/com/cloudwick/kafka/producer/TestProducer.java
@@ -3,7 +3,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+
 
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
@@ -11,48 +13,56 @@ import kafka.producer.ProducerConfig;
 
 public class TestProducer {
 	public static void main(String[] args) throws InterruptedException, FileNotFoundException {
-		if(args.length != 2)
+		if(args.length != 4)
 		{
-			System.out.println("Usage: java -cp KafkaProducerSample-0.0.1-SNAPSHOT-jar-with-dependencies.jar <num_of_events> <event_interval_in_ms>");
+			System.out.println("Usage: java -cp KafkaProducerSample-0.0.1-SNAPSHOT-jar-with-dependencies.jar  <kafka-broker> <topics_seperated_by_comma> <num_of_events> <event_interval_in_ms>");
 			System.exit(1);
 		}
-
-		long events = Long.parseLong(args[0]);
-		Integer interval = Integer.parseInt(args[1]);
-		BufferedReader br = new BufferedReader(new FileReader("Sample_Data_generator.csv"));
 		
-		Random rdm = new Random();
+		//initializing variables
+		long events = Long.parseLong(args[2]);
+		Integer interval = Integer.parseInt(args[3]);
+		String[] topics = args[1].split(",");
+		BufferedReader br = new BufferedReader(new InputStreamReader(TestProducer.class.getResourceAsStream("/sampleset.txt")));
+		//setting up properties to be used to communicate to kafka
 		Properties props = new Properties();
-		props.put("metadata.broker.list", "172.16.6.92:6667");
+		props.put("metadata.broker.list", args[0].toString());
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
 		props.put("request.required.acks", "1");
 		ProducerConfig config = new ProducerConfig(props);
 		Producer<String, String> producer = new Producer<String, String>(config);
+		
 		String line = "";
-		String cvsSplitBy = ",";
-		int i=0;
+		String csvSplitBy = ",";
+		Random rdm = new Random();
 		ArrayList<List<String>> userdata = new ArrayList<>();
+		
 		try{
+			//loading the data as Arrays of Lists to be able to pick different cells for generating random data from sample set
 			while ((line = br.readLine()) != null) {
-				String[] temp = line.split(cvsSplitBy);
+				String[] temp = line.split(csvSplitBy);
 				userdata.add(Arrays.asList(temp));
-				i++;
 			}
+			//getting the size of the Array to use as random seed so that we wont run into ArrayIndexOutOfBounds Exception
 			int size = userdata.size();
 
-			for (long nEvents = 0; nEvents < events; nEvents++) 
+			//creating the data and sending it
+			for (long nEvents = 1; nEvents <= events; nEvents++) 
 			{
-				String msg = 
-						userdata.get(rdm.nextInt(size)).get(0).toString().trim()+","+
-						userdata.get(rdm.nextInt(size)).get(1).toString().trim()+","+
-						userdata.get(rdm.nextInt(size)).get(2).toString().trim()+","+
-						userdata.get(rdm.nextInt(size)).get(3).toString().trim()+","+
-						userdata.get(rdm.nextInt(size)).get(4).toString().trim()+","+
-						userdata.get(rdm.nextInt(size)).get(5).toString().replace("$", " ").trim()+","+
-						userdata.get(rdm.nextInt(size)).get(6).toString().trim();
-
-				KeyedMessage<String, String> data = new KeyedMessage<String, String>("user_data", new Date().getTime()+"", msg);
+				String msg = "{"+
+						"\"StockSymbol\":"+userdata.get(rdm.nextInt(size)).get(0).toString().trim()+","+
+						"\"StockNumber\":"+userdata.get(rdm.nextInt(size)).get(1).toString().trim()+","+
+						"\"Mp\":"+userdata.get(rdm.nextInt(size)).get(2).toString().trim()+","+
+						"\"Bp\":"+userdata.get(rdm.nextInt(size)).get(3).toString().trim()+","+
+						"\"Ap\":"+userdata.get(rdm.nextInt(size)).get(4).toString().trim()+","+
+						"\"BQ\":"+userdata.get(rdm.nextInt(size)).get(5).toString().trim()+","+
+						"\"Aq\":"+userdata.get(rdm.nextInt(size)).get(6).toString().trim()+","+
+						"\"Vol\":"+userdata.get(rdm.nextInt(size)).get(7).toString().trim()+","+
+						"\"rowId\":"+userdata.get(rdm.nextInt(size)).get(8).toString().trim()
+						+"}";
+				
 				System.out.println(msg);
+				KeyedMessage<String, String> data = new KeyedMessage<String, String>(topics[0], new Date().getTime()+"", msg);
 				producer.send(data);
 				Thread.sleep(interval);
 			}
